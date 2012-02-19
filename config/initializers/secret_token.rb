50e5f7a38fa8f01170cdb107723a9a71e1cd87ea
valeron <t-gult_23@hotmail.com>
2012-02-19 19:46:26 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ikkaku::Application.config.secret_token = '5e20e67e982655c687341474981496289a594f249e1b3ddb61ba95a5c66a92a3be66b4f21a2a1737f066076ee393ecf4606283d7e488ea167ebf69992041eb14'
